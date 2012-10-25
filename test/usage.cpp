5da9af5092c318151a6e909413b57406ebb32a95
Rowan James <rowanj@phere.net>
2012-10-25 16:34:59 +1100

    Start to flesh out usage tests

--- test/usage.cpp
+++ test/usage.cpp
@@ -1,15 +1,15 @@
-#define BOOST_TEST_MODULE smart_ptr
+#define BOOST_TEST_MODULE shared_ptr
 #include <boost/test/unit_test.hpp>
 
 #include <phere/shared_ptr.hpp>
-#include <phere/shared_ptr.hpp>
+#include <boost/smart_ptr/make_shared.hpp>
 
 #include <iostream>
 #include <string>
 
 namespace
 {
-  static const bool printTracking = false;
+  static const bool printTracking = true;
 }
 
 struct Tracker
@@ -38,6 +38,11 @@ struct Tracker
     }
   }
 
+  std::string getName() const
+  {
+	return name;
+  }
+
 private:
   static size_t created;
   static size_t alive;
@@ -51,5 +56,17 @@ size_t Tracker::alive = 0;
 
 BOOST_AUTO_TEST_CASE( usage )
 {
-  //  Tracker tracker("Foo");
+  typedef phere::shared_ptr<Tracker> TrackerPtr;
+
+  TrackerPtr p1(boost::make_shared<Tracker>("p1"));
+  TrackerPtr p2(p1);
+  TrackerPtr p3 = p2;
+
+  BOOST_CHECK_EQUAL(p1, p2);
+  BOOST_CHECK(p2.pointsToSameObject(p3));
+
+  BOOST_CHECK(p3); // conversion to bool
+  BOOST_CHECK_EQUAL(p3->getName(), "p1");
+  p3.reset();
+  BOOST_CHECK(!p3);
 }
